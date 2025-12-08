89440c72a5b3efcca9dfbe8860d5fca995a0b34e
Ayushi-aashish <ayushiaashish865@gmail.com>
2025-12-08 22:57:24 +0530

    Project entity (depends on Account)

---
+++ src/account/models/project.entity.ts
@@ -0,0 +1,64 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Account } from '../../account/models/account.entity';
+
+export enum ProjectProgress {
+  NOT_STARTED = 'not_started',
+  WORKING = 'working',
+  FINISHED = 'finished',
+}
+
+export enum ProjectUrgency {
+  MINIMAL = 'minimal',
+  NORMAL = 'normal',
+  CRITICAL = 'critical',
+}
+
+@Entity('projects')
+export class Project {
+  @PrimaryGeneratedColumn('uuid')
+  projectId: string;
+
+  @Column()
+  projectName: string;
+
+  @Column({ type: 'text', nullable: true })
+  projectDetails: string;
+
+  @Column({
+    type: 'enum',
+    enum: ProjectProgress,
+    default: ProjectProgress.NOT_STARTED,
+  })
+  progress: ProjectProgress;
+
+  @Column({
+    type: 'enum',
+    enum: ProjectUrgency,
+    default: ProjectUrgency.NORMAL,
+  })
+  urgency: ProjectUrgency;
+
+  @Column({ type: 'date', nullable: true })
+  targetDate: Date;
+
+  @Column()
+  ownerId: string;
+
+  @ManyToOne(() => Account, (account) => account.projects, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'ownerId' })
+  owner: Account;
+
+  @CreateDateColumn()
+  initiatedAt: Date;
+
+  @UpdateDateColumn()
+  lastUpdatedAt: Date;
+}
